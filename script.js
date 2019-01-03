e7956809725039f3f92c02db43165330b57e3dec
AndrewPeralta11 <43342098+AndrewPeralta11@users.noreply.github.com>
2019-01-03 16:50:32 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,30 @@
+$("#thanos").click(function(){
+ $("#thanos").hide();
+ $("#thanosSnap").show();
+});
+
+
+//Spell #1
+
+$("#spiderman").click(function(){
+ $(".snap1").fadeOut();
+});
+
+
+
+//Spell #2
+
+$("#warMachine").click(function(){
+ $(".snap2").hide();
+});
+
+
+
+//Spell #3
+
+$("#blackPanther").click(function(){
+ $(".snap3").slideUp();
+});
+
+
+
